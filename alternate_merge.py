7ce46c796295f2fa5270481fec264c57f30144ae
Aathira S <saathira2004@gmail.com>
2025-07-02 22:53:43 +0530

    Add files via upload

---
+++ alternate_merge.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        merge=[]
+        i,j=0,0
+        len1,len2=len(word1),len(word2)
+
+        while i<len1 and j<len2:
+            merge.append(word1[i])
+            merge.append(word2[j])
+            i+=1
+            j+=1
+
+        if i<len1:
+            merge.append(word1[i:])
+        if j<len2:
+            merge.append(word2[j:])
+        return ''.join(merge) 
