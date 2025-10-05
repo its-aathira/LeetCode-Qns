f2ddd1442f41f7d818367e594289c28eb693f920
Aathira S <saathira2004@gmail.com>
2025-10-05 23:16:14 +0530

    Add files via upload

---
+++ 605. Can place flowers.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        length=len(flowerbed)
+        count=0
+        for i in range (length):
+            if flowerbed[i]==0:
+                empty_left=(i==0) or (flowerbed[i-1]==0)
+                empty_right=(i==length-1) or (flowerbed[i+1]==0)
+                if empty_left and empty_right:
+                    flowerbed[i]=1
+                    count+=1
+                    if count>=n:
+                        return True
+        return count>=n
